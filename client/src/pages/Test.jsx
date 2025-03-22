04a6aaee7c873240aa5cc4d832853cce57eb7d81
Maaz-Shaikh-413 <shaikhmaaz8788@gmail.com>
2025-03-22 17:33:56 +0530

    test2

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Test = () => {
   return (
-    <div>Test</div>
+    <>
+        <div>Test</div>
+        <div>Test2</div>
+        <div>Test3</div>
+    </>
+
   )
 }

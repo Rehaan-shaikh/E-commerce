0dcf28d4806ea9a559038b2efd04741e7641153b
1Alraza <alrazaquadri62@gmail.com>
2025-04-07 02:40:39 +0530

    header complete

--- client/src/components/shopping-view/Header.jsx
+++ client/src/components/shopping-view/Header.jsx
@@ -1,11 +1,119 @@
-import React from 'react'
+import { HousePlug, LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import {
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { logoutUser } from "@/store/auth-slice";
+import { shoppingViewHeaderMenuItems } from "@/config";
 
-const ShoppingHeader = () => {
-  return (
-    <div>
-      Shopping header
-    </div>
-  )
+function MenuItems(){
+  return <nav className="flex flex-col mb-3 bg lg:mb-0 lg:items-center gap-6 lg:flex-row">
+    {
+      shoppingViewHeaderMenuItems.map(menuItem=><Link className="text-sm font-medium cursor-pointer" key={menuItem.id} to={menuItem.path} >{menuItem.label}</Link>)
+    }
+
+  </nav>
 }
 
-export default ShoppingHeader
+function HeaderRightContent() {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
+
+
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Button variant="outline" size="icon" className="relative">
+        <ShoppingCart className="w-6 h-6" />  
+        <span className="sr-only">User Cart</span>
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent side="right" className="w-56">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            onClick={() => navigate("/shop/account")}
+            className="flex items-center gap-2"
+          >
+            <UserCog className="h-4 w-4" />
+            <span>Account</span>
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            onClick={handleLogout}
+            className="flex items-center gap-2"
+          >
+            <LogOut className="h-4 w-4" />
+            <span>Logout</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+    </div>
+  );
+}
+
+
+function ShoppingHeader() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default ShoppingHeader;

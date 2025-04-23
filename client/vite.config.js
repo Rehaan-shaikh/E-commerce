dfbb2814ae5e424d196f3acef1dfb299224a28a1
Maaz-Shaikh-413 <shaikhmaaz8788@gmail.com>
2025-04-23 20:07:02 +0530

    more changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: './', // ✅ Add this line for correct asset paths on Render
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
